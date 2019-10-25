198c7d70fc335f94b44a048e3f57c748d4c94b7c
ljtnono <ljtnono@163.com>
2019-10-26 02:12:07 +0800

    完成side部分内容

--- src/main/resources/static/fore/js/common.js
+++ src/main/resources/static/fore/js/common.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// 计算滚动时页面
+const $scroll_fixed = $("[scroll-fixed]");
 /**监听鼠标滚动事件*/
 $(window).scroll(function(e){
     let scrollTop = $(document).scrollTop();
@@ -8,6 +10,21 @@ $(window).scroll(function(e){
     } else {
         $(".roll-top").fadeOut(800);
     }
+    // 设置侧边栏鼠标滚动停靠
+    $scroll_fixed.each(function (e, obj) {
+        console.log($(obj).offset().top);
+    });
+    // console.log(offsetTops);
+    // offsetTops.each(function (e) {
+    //     console.log(e);
+    //     if (scrollTop > tagsOffsetTop) {
+    //         $(".tags:eq(0)").css("position", "fixed").css("top", "20px");
+    //         $(".links:eq(0)").css("position", "fixed").css("top", "80px");
+    //     } else {
+    //         $(".tags:eq(0)").css("position", "");
+    //         $(".links:eq(0)").css("position", "");
+    //     }
+    // });
 });
 
 /**鼠标点击回到顶部事件*/
