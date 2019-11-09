8973fda59b5efdbcac564563427ca8f4dc653720
ljtnono <ljtnono@163.com>
2019-11-09 23:52:21 +0800

    添加ReConfigMapper

--- src/main/java/cn/ljtnono/re/mapper/ReBlogMapper.java
+++ src/main/java/cn/ljtnono/re/mapper/ReBlogMapper.java
@@ -2,6 +2,8 @@ package cn.ljtnono.re.mapper;
 
 import cn.ljtnono.re.entity.ReBlog;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 /**
  * <p>
