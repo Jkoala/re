f9a45916b6627e90171708fd33869f64c915711f
ljtnono <ljtnono@163.com>
2019-11-11 23:26:05 +0800

    首页部分流加载写完

--- src/main/resources/static/fore/js/utils.js
+++ src/main/resources/static/fore/js/utils.js
@@ -3,7 +3,7 @@
  */
 "use strict";
 
-const dataFormatter = {
+const dateFormatter = {
     DEFAULT_DATETIME_FORMAT: "yyyy-MM-dd HH:mm:ss",
     DEFAULT_TIME_FORMAT: "HH:mm:ss",
     DEFAULT_DATE_FORMAT: "yyyy-MM-dd"
@@ -13,17 +13,19 @@ function DateUtil() {}
 
 DateUtil.prototype = {
 
-    format: function(formatter) {
-        let d = new Date();
+    format: function(formatter, d) {
+        if (!d) {
+            d = new Date();
+        }
         let year = d.getFullYear();
         let month = d.getMonth() + 1;
         let date = d.getDate();
         let hours = d.getHours();
         let minutes = d.getMinutes();
         let seconds = d.getSeconds();
-        if (formatter === dataFormatter.DEFAULT_DATETIME_FORMAT) {
+        if (formatter === dateFormatter.DEFAULT_DATETIME_FORMAT) {
             return year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
-        } else if (formatter === dataFormatter.DEFAULT_DATE_FORMAT) {
+        } else if (formatter === dateFormatter.DEFAULT_DATE_FORMAT) {
             return year + "-" + month + "-" + date;
         } else if(true) {
             return "";
