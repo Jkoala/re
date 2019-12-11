2fce05b5589246de7fa4e1beedd0897ade3f3bea
ljtnono <ljtnono@163.com>
2019-12-11 17:55:33 +0800

    修改entity改为使用lombok简化操作

---
+++ src/main/java/cn/ljtnono/re/entity/common/BaseEntity.java
@@ -0,0 +1,29 @@
+package cn.ljtnono.re.entity.common;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Date;
+
+/**
+ * 所有实体类的公共字段
+ * @author ljt
+ * @date 2019/12/11
+ * @version 1.0
+ */
+@Data
+@SuperBuilder
+public class BaseEntity {
+
+    /** 创建时间 */
+    protected Date createTime;
+
+    /** 最后修改时间 */
+    protected Date modifyTime;
+
+    /** 状态 0已经删除 1正常 */
+    @TableField("`delete`")
+    protected byte delete;
+
+}
