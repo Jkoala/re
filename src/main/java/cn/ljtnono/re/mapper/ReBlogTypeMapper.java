60b053de548753a158d4a1c5e240141466c911dd
ljtnono <ljtnono@163.com>
2019-12-23 17:37:54 +0800

    修改注释

--- src/main/java/cn/ljtnono/re/mapper/ReBlogTypeMapper.java
+++ src/main/java/cn/ljtnono/re/mapper/ReBlogTypeMapper.java
@@ -1,15 +1,15 @@
 package cn.ljtnono.re.mapper;
 
 import cn.ljtnono.re.entity.ReBlogType;
+import cn.ljtnono.re.pojo.JsonResult;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Update;
 
 /**
- * <p>
- *  Mapper 接口
- * </p>
- *
+ * 博客类型mapper接口
  * @author ljt
- * @since 2019-10-06
+ * @date 2019/12/23
+ * @version 1.0.0
  */
 public interface ReBlogTypeMapper extends BaseMapper<ReBlogType> {
 
