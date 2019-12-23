54371bb0f5ecfeab2e26360f23c32e55ecc59f92
ljtnono <ljtnono@163.com>
2019-12-23 17:37:16 +0800

    修改版本号记录方式, 改成构造函数注入方式

--- src/main/java/cn/ljtnono/re/controller/ReBlogTypeController.java
+++ src/main/java/cn/ljtnono/re/controller/ReBlogTypeController.java
@@ -1,9 +1,7 @@
 package cn.ljtnono.re.controller;
 
-
 import cn.ljtnono.re.dto.PageDTO;
 import cn.ljtnono.re.entity.ReBlogType;
-import cn.ljtnono.re.enumeration.GlobalErrorEnum;
 import cn.ljtnono.re.pojo.JsonResult;
 import cn.ljtnono.re.service.IReBlogTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,98 +14,55 @@ import java.io.Serializable;
 /**
  * 博客类型Controller
  * @author ljt
- * @date 2019/11/18
- * @version 1.0
+ * @date 2019/12/23
+ * @version 1.0.1
  */
 @RestController
 @RequestMapping("/blog_type")
 public class ReBlogTypeController extends AbstractReController<ReBlogType> {
 
-    @Autowired
     private IReBlogTypeService iReBlogTypeService;
 
-    /**
-     * 获取实体类的所有列表
-     *
-     * @return 实体类所有列表
-     * 操作成功{request: "success", status: 200, message: "操作成功“, data: {列表}}
-     * 操作失败{request: "fail", status: 具体错误码{@link GlobalErrorEnum}, message: 具体错误信息{@link GlobalErrorEnum}}
-     */
+    @Autowired
+    public ReBlogTypeController(IReBlogTypeService iReBlogTypeService) {
+        this.iReBlogTypeService = iReBlogTypeService;
+    }
+
     @Override
     @GetMapping
     public JsonResult listEntityAll() {
         return iReBlogTypeService.listEntityAll();
     }
 
-    /**
-     * 新增单个实体类
-     *
-     * @param entity 具体的实体类
-     * @return 返回操作结果
-     * 操作成功返回（如果有附加信息，那么通过fields字段带回，其中特别注意如果data为null，那么不返回)
-     * {request: "success", status: 200, message: "操作成功“}
-     * 操作失败返回
-     * {request: "fail", status: 具体错误码{@link GlobalErrorEnum}, message: 具体错误信息{@link GlobalErrorEnum}}
-     */
+
     @Override
     @PostMapping
     public JsonResult saveEntity(ReBlogType entity) {
-        return null;
+        return iReBlogTypeService.saveEntity(entity);
     }
 
-    /**
-     * 根据id更新一个实体类
-     *
-     * @param id 实体类的id
-     * @param entity 需要更新的实体类
-     * @return 返回操作结果
-     * 操作成功返回（如果有附加信息，那么通过fields字段带回，其中特别注意如果data为null，那么不返回)
-     * {request: "success", status: 200, message: "操作成功“}
-     * 操作失败返回
-     * {request: "fail", status: 具体错误码{@link GlobalErrorEnum}, message: 具体错误信息{@link GlobalErrorEnum}}
-     */
+
     @Override
-    public JsonResult updateEntityById(Serializable id, ReBlogType entity) {
-        return null;
+    @PostMapping("/{id}")
+    public JsonResult updateEntityById(@PathVariable(value = "id", required = false) Serializable id, ReBlogType entity) {
+        return iReBlogTypeService.updateEntityById(id, entity);
     }
 
-    /**
-     * 根据id删除一个实体类
-     *
-     * @param id 实体类id
-     * @return 返回操作结果
-     * 操作成功返回（如果有附加信息，那么通过fields字段带回，其中特别注意如果data为null，那么不返回)
-     * {request: "success", status: 200, message: "操作成功“, data: {删除的实体类}}
-     * 操作失败返回
-     * {request: "fail", status: 具体错误码{@link GlobalErrorEnum}, message: 具体错误信息{@link GlobalErrorEnum}}
-     */
+
     @Override
     @DeleteMapping("/{id}")
     public JsonResult deleteEntityById(@PathVariable(value = "id", required = false) Serializable id) {
         return iReBlogTypeService.deleteEntityById(id);
     }
 
-    /**
-     * 根据id获取一个实体类
-     *
-     * @param id 实体类id
-     * @return 返回操作结果
-     * 操作成功返回（如果有附加信息，那么通过fields字段带回，其中特别注意如果data为null，那么不返回)
-     * {request: "success", status: 200, message: "操作成功“, data: {实体类}}
-     * 操作失败返回
-     * {request: "fail", status: 具体错误码{@link GlobalErrorEnum}, message: 具体错误信息{@link GlobalErrorEnum}}
-     */
+
     @Override
-    public JsonResult getEntityById(Serializable id) {
-        return null;
+    @GetMapping("/{id}")
+    public JsonResult getEntityById(@PathVariable(value = "id", required = false) Serializable id) {
+        return iReBlogTypeService.getEntityById(id);
     }
 
 
-    /**
-     * 分页查询，如果分页查询带有type参数，那么是按照类型的分页查询
-     * @param pageDTO 分页DTO对象
-     * @return JsonResult 格式对象
-     */
     @GetMapping("/listBlogTypePage")
     public JsonResult listBlogTypePage(PageDTO pageDTO) {
         return iReBlogTypeService.listBlogTypePage(pageDTO.getPage(), pageDTO.getCount());
